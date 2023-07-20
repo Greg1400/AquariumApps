dfcea8fd192b4b8edcdcea77ed522f248a7cc4e4
Greg1400 <herzig.gregoire@gmail.com>
2023-07-20 12:22:31 +0200

    Add function createAquarium into Aquarium swift file

--- Sources/Aquarium/Models/Aquarium.swift
+++ Sources/Aquarium/Models/Aquarium.swift
@@ -10,9 +10,30 @@ class Aquarium {
         self.capacity = capacity
         self.waterType = waterType
     }
+
+    // Function to create an aquarium object
+    static func createAquarium() -> Aquarium {
+        // Prompt the user for aquarium capacity
+        print("Enter the aquarium's capacity (in liters):")
+        if let capacityString = readLine(), let capacity = Int(capacityString) {
+            // Prompt the user to choose water type
+            print("Choose the water type (freshwater or saltwater):")
+            if let waterTypeString = readLine()?.lowercased(), let waterType = WaterType(rawValue: waterTypeString) {
+                // Create and return the aquarium object
+                return Aquarium(capacity: capacity, waterType: waterType)
+            } else {
+                print("Invalid water type. The aquarium was not created.")
+            }
+        } else {
+            print("Invalid capacity. The aquarium was not created.")
+        }
+        
+        // Return a default aquarium if input is invalid
+        return Aquarium(capacity: 50, waterType: .freshwater)
+    }
 }
 
-enum WaterType {
+enum WaterType: String {
     case freshwater
     case saltwater
 }
