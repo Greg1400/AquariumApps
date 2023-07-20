1c46e45d20476c7484d5e58f9c7fce33f58ad2c3
Greg1400 <herzig.gregoire@gmail.com>
2023-07-20 14:01:10 +0200

    Add code for temparture and fishlibrary

--- Sources/Aquarium/Controllers/TemparatureController.swift
+++ Sources/Aquarium/Controllers/TemparatureController.swift
@@ -12,5 +12,31 @@ class TemperatureController {
     func createTemperatureGraph() {
         print("Creating temperature graph...")
         // Code pour créer un graphique de température basé sur les données de l'aquarium
+
+        //For Demonstration purposes, let's assume we have some temprature dats
+        let temperatureData: [Double] = [25.0,26.0,24.5,23.8,25.5,26.5]
+// Assuming you have a graphing library or custom drawing logic to create the graph
+        // Draw the graph with the temperature data and the ideal temperature range (20.0°C to 25.0°C)
+        print("Temperature graph:")
+        for (index, temperature) in temperatureData.enumerated() {
+            let date = Calendar.current.date(byAdding: .day, value: -6 + index, to: Date())!
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .short
+            let dateString = dateFormatter.string(from: date)
+            print("\(dateString): \(temperature)°C")
+        }
+        
+        let idealTemperatureMin = 20.0
+        let idealTemperatureMax = 25.0
+        print("Ideal Temperature Range: \(idealTemperatureMin)°C to \(idealTemperatureMax)°C")
+        
+        // Assuming you have a method to draw a dotted line on the graph representing the ideal temperature range
+        drawDottedLineOnGraph(idealTemperatureMin: idealTemperatureMin, idealTemperatureMax: idealTemperatureMax)
+    }
+    
+    // Method to draw a dotted line on the graph representing the ideal temperature range
+    private func drawDottedLineOnGraph(idealTemperatureMin: Double, idealTemperatureMax: Double) {
+        // Code to draw a dotted line on the graph using the graphing library or custom drawing logic
+        print("Dotted line drawn on the graph representing the ideal temperature range.")
     }
 }
--- Sources/Aquarium/Views/FishLibraryView.swift
+++ Sources/Aquarium/Views/FishLibraryView.swift
@@ -11,7 +11,48 @@ class FishLibraryView {
     
     func searchFish() -> Fish? {
         print("Searching for a fish...")
-        // Code pour rechercher des poissons dans la bibliothèque
-        return fishLibrary.fishes.first
+        // Code to search for a fish in the library
+        
+        // For demonstration purposes, let's assume you have the name of the fish from user input
+        print("Enter the name of the fish:")
+        if let fishName = readLine() {
+            // Load the fish data from the JSON file
+            if let fishData = loadFishDataFromJSON() {
+                // Search for the fish by name
+                let foundFish = fishData.first { $0.commonName.lowercased() == fishName.lowercased() }
+                if let fish = foundFish {
+                    print("Found fish: \(fish.commonName) (\(fish.species))")
+                    return fish
+                } else {
+                    print("Fish not found.")
+                }
+            } else {
+                print("Error loading fish data from JSON file.")
+            }
+        } else {
+            print("Invalid input.")
+        }
+        
+        return nil
+    }
+    
+    // Method to load fish data from the JSON file
+    private func loadFishDataFromJSON() -> [Fish]? {
+        // Load the JSON file from the specified path (assuming "FishName.json" is in the app bundle)
+        if let jsonURL = Bundle.main.url(forResource: "FishName", withExtension: "json") {
+            do {
+                let jsonData = try Data(contentsOf: jsonURL)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let fishData = try decoder.decode([Fish].self, from: jsonData)
+                return fishData
+            } catch {
+                print("Error decoding fish data from JSON: \(error)")
+                return nil
+            }
+        } else {
+            print("Fish data JSON file not found.")
+            return nil
+        }
     }
 }
